9bc316901e4ca2c42a16874732aee7b29772cad6
techlover <e.rono@outlook.com>
2023-09-13 20:35:16 +0300

    controllers Update

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,16 +1,38 @@
 class JobsController < ApplicationController
+  before_action :require_login, except: [:index, :show]
+
+  # ...
+
   def index
+    @jobs = Job.all
   end
 
   def new
+    @job = Job.new
   end
 
   def create
+    @job = current_user.jobs.build(job_params)
+    if @job.save
+      redirect_to @job, notice: 'Job was successfully created.'
+    else
+      render 'new'
+    end
   end
 
   def show
+    @job = Job.find(params[:id])
   end
 
   def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_path, notice: 'Job was successfully deleted.'
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
   end
 end
