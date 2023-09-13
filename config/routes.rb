1b2896b7d35bc2340dc8d3a14b2bd95df3c06dd2
techlover <e.rono@outlook.com>
2023-09-13 19:39:28 +0300

    updated files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'applications/new'
+  get 'applications/create'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  get 'jobs/index'
+  get 'jobs/new'
+  get 'jobs/create'
+  get 'jobs/show'
+  get 'jobs/destroy'
   get 'users/new'
   get 'users/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_13_161840) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_13_162835) do
+  create_table "jobs", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "session_token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
